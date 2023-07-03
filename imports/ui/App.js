05c56045cb7a93f216bb565c6921b430a78aecf5
Shaun Wong <shaunwong@Shauns-Mac-mini.local>
2023-07-03 15:30:33 +0800

    add logout button, attach userid to task collection

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -1,66 +1,100 @@
+import { Meteor } from 'meteor/meteor';
 import { Template } from 'meteor/templating';
-import { TasksCollection } from "../api/TasksCollection";
+import { TasksCollection } from '../api/TasksCollection';
 import { ReactiveDict } from 'meteor/reactive-dict';
-
 import './App.html';
 import './Task.js';
-import "./Login.js";
+import './Login.js';
+
+const HIDE_COMPLETED_STRING = 'hideCompleted';
+
+const getUser = () => Meteor.user();
+const isUserLogged = () => !!getUser();
+
+const getTasksFilter = () => {
+  const user = getUser();
+
+  const hideCompletedFilter = { isChecked: { $ne: true } };
+
+  const userFilter = user ? { userId: user._id } : {};
+
+  const pendingOnlyFilter = { ...hideCompletedFilter, ...userFilter };
+
+  return { userFilter, pendingOnlyFilter };
+};
 
 Template.mainContainer.onCreated(function mainContainerOnCreated() {
   this.state = new ReactiveDict();
 });
 
-const HIDE_COMPLETED_STRING = "hideCompleted";
-
 Template.mainContainer.events({
-  "click #hide-completed-button"(event, instance) {
+  'click #hide-completed-button'(event, instance) {
     const currentHideCompleted = instance.state.get(HIDE_COMPLETED_STRING);
     instance.state.set(HIDE_COMPLETED_STRING, !currentHideCompleted);
-  }
+  },
+  'click .user'() {
+    Meteor.logout();
+  },
 });
 
-const getUser = () => Meteor.user();
-const isUserLogged = () => !!getUser();
-
 Template.mainContainer.helpers({
-  isUserLogged() {
-    return isUserLogged();
-  },
   tasks() {
     const instance = Template.instance();
     const hideCompleted = instance.state.get(HIDE_COMPLETED_STRING);
-    const hideCompletedFilter = { isChecked: { $ne: true } };
 
-    return TasksCollection.find(hideCompleted ? hideCompletedFilter : {}, {
-      sort: { createdAt: -1 },
-    }).fetch();
+    const { pendingOnlyFilter, userFilter } = getTasksFilter();
+
+    if (!isUserLogged()) {
+      return [];
+    }
+
+    return TasksCollection.find(
+      hideCompleted ? pendingOnlyFilter : userFilter,
+      {
+        sort: { createdAt: -1 },
+      }
+    ).fetch();
   },
   hideCompleted() {
     return Template.instance().state.get(HIDE_COMPLETED_STRING);
   },
   incompleteCount() {
-    // we are filtering the results here
-    const incompleteTasksCount = TasksCollection.find({ isChecked: { $ne: true } }).count();
+    if (!isUserLogged()) {
+      return '';
+    }
+
+    const { pendingOnlyFilter } = getTasksFilter();
+
+    const incompleteTasksCount = TasksCollection.find(
+      pendingOnlyFilter
+    ).count();
     return incompleteTasksCount ? `(${incompleteTasksCount})` : '';
+  },
+  isUserLogged() {
+    return isUserLogged();
+  },
+  getUser() {
+    return getUser();
   },
 });
 
 Template.form.events({
-  "submit .task-form"(event) {
+  'submit .task-form'(event) {
     // Prevent default browser form submit
     event.preventDefault();
 
     // Get value from form element
-    const target = event.target;
+    const { target } = event;
     const text = target.text.value;
 
     // Insert a task into the collection
     TasksCollection.insert({
       text,
+      userId: getUser()._id,
       createdAt: new Date(), // current time
     });
 
     // Clear form
     target.text.value = '';
-  }
-})
+  },
+});
